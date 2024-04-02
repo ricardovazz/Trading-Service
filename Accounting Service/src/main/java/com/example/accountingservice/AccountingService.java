9765c53d9240816bc984ad961f9b4825c176e880
ricardo.vaz <ricardo.vaz@gft.com>
2024-04-02 15:12:42 +0100

    updated acc svc

--- Accounting Service/src/main/java/com/example/accountingservice/AccountingService.java
+++ Accounting Service/src/main/java/com/example/accountingservice/AccountingService.java
@@ -1,6 +1,8 @@
 package com.example.accountingservice;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -9,16 +11,24 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Service
+@AllArgsConstructor
 public class AccountingService {
     private final Map<Long, TradeConfirmation> confirmations = new ConcurrentHashMap<>();
 
-    public Mono<TradeConfirmation> confirmTrade(Long tradeId, TradeConfirmation tradeConfirmation) {
+    private final WebClient webClient;
+
+    public Mono<Settlement> confirmTrade(Long tradeId, TradeConfirmation tradeConfirmation) {
 
         tradeConfirmation.setTradeId(tradeId);
         tradeConfirmation.setStatus("CONFIRMED");
         tradeConfirmation.setConfirmationTimestamp(LocalDateTime.now());
         confirmations.put(tradeId, tradeConfirmation);
-        return Mono.just(tradeConfirmation);
+
+        return webClient.post()
+                .uri("http://settlement-service/confirmations")
+                .bodyValue(new Settlement(null, tradeId, tradeConfirmation.getConfirmedQuantity(), tradeConfirmation.getPrice(),tradeConfirmation.getConfirmationTimestamp()))
+                .retrieve()
+                .bodyToMono(Settlement.class);
     }
 
     public Flux<TradeConfirmation> getAllConfirmations() {
